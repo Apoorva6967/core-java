acd90a8e5c9b7ee56e97015762dc10002daac436
Apoorva6967 <apoorva02.xworkz@gmail.com>
2022-11-23 20:46:17 +0530

    Commiting Java files

---
+++ Cricket.java
@@ -0,0 +1,7 @@
+class Cricket{
+	static String playersName[] = {"Virat" , "MS Dhoni" , "Rohit"};
+	public static void main(String cricket[]){
+		System.out.println(playersName.length);
+		System.out.println(playersName[0] + "  "+ playersName[1] + " " + playersName[2]);
+	}
+}
---
+++ bottles.java
@@ -0,0 +1,7 @@
+class bottles{
+
+	public static void main(String bottles[]){
+		String bottlesName = "Water bottle" ;
+		System.out.println("Type of bottle is" + bottlesName);
+	}
+}
